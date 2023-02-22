32384b50421057a86cfd71839a4ea394195e7389
wmvieira21 <90009567+wmvieira21@users.noreply.github.com>
2023-02-21 23:17:53 -0300

    Animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { trigger, state, transition, style, animate } from '@angular/animations';
-import { Component } from '@angular/core';
+import { trigger, state, transition, style, animate, animation, keyframes } from '@angular/animations';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -20,43 +21,95 @@ import { Component } from '@angular/core';
     ]),
 
     trigger('wildState', [
+
       state('normal', style({
-        'background-color': 'yellow',
+        'background-color': 'grey',
         transform: 'translateX(0) scale(1)'
       })),
+
       state('highlight', style({
         'background-color': 'blue',
         transform: 'translateX(100px) scale(1)'
       })),
+
       state('shrunken', style({
         'background-color': 'green',
-        transform: 'translateX(100px) scale(0.5)'
+        transform: 'translateX(0) scale(0.5)'
       })),
-      transition('normal => highlight', animate(300)),
-      transition('highlight => normal', animate(800)),
 
-      transitionn('shrunken <=> *', [
-        animate(300, style({
-          borderRadius: '50px'
-        })),
+      transition('normal <=> highlight', animate(300)),
+
+      transition('shrunken <=>*', [
+        style({ 'background-color': 'black' }),
+        animate(3000, keyframes(
+          [
+            style({ 'border-radius': '50px' })
+          ])),
+        animate(2000)
+      ])
+    ]),
+
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void =>*', [
         style({
-          'background-color': 'black'
-        })
-      ],
-        animate(500))
+          opacity: 0,
+          transform: 'translateX(-150px)'
+        }),
+        animate(1000)
+      ]),
+      transition('* => void', animate(1000,
+        style({
+          opacity: 0,
+          transform: 'translateX(300px)'
+        }))
+      )
+    ]),
+
+    trigger('list2', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void =>*', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-150px)'
+        }),
+        animate(1000)
+      ]),
+      transition('* => void', animate(1000,
+        style({
+          opacity: 0,
+          transform: 'translateX(300px)'
+        }))
+      )
     ])
   ]
 })
+
 export class AppComponent {
   title = 'Angular-Animation';
   state = 'normal';
   wildState = 'normal';
+  list = ['milk', 'suger', 'bread'];
+  @ViewChild('ingredient') ingredient: ElementRef;
 
   onAnimation() {
-    this.state = (this.state === 'normal' ? 'highlight' : 'normal');
-    this.wildState = (this.wildState === 'normal' ? 'highlight' : 'normal');
+    this.state = (this.state == 'normal' ? 'highlight' : 'normal');
+    this.wildState = (this.wildState == 'normal' ? 'highlight' : 'normal');
   }
   onShrink() {
     this.wildState = 'shrunken';
+  }
+
+  onAddIngredient() {
+    this.list.push(this.ingredient.nativeElement.value);
+  }
+  deleteItem(index: number) {
+    this.list.splice(index, 1);
   }
 }
